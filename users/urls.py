189d2624553c566b0616a218ff508503964a5e1c
Anthony Abeo <anthonyabeo@gmail.com>
2018-09-18 11:43:34 +0000

    add docx for registration view and validate data function

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import url
 from users.views import registration, sign_in, sign_out
 
 urlpatterns = [
-
     url(r'^registration/$', registration, name="registration"),
     url(r'^login/$', sign_in, name="login"),
     url(r'^logout/$', sign_out, name="logout"),
--- users/utils.py
+++ users/utils.py
@@ -2,6 +2,21 @@ import re
 
 
 def validate_data(username, password, email, first_name, last_name, user_type):
+    """Validate the user data provided
+
+    Checks the user information provided during registration
+    to ensure that the information is in the appropriate
+    format before proceeding.
+
+    :param username: username of the potential
+    :param password: password of the potential
+    :param email: email address of the potential
+    :param first_name: first name of the potential
+    :param last_name: last name of the potential
+    :param user_type: authorization access of the potential
+    :return ok: boolean indicating whether or not there is invalid data
+            msg: list of messages to be sent to the user if there were errors
+    """
     ok, messages = True, []
 
     if first_name is None or len(first_name) < 1:
