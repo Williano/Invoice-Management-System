121bdd2c238e67f7bd9e3021736b4c310eb64486
Anthony Abeo <anthonyabeo@gmail.com>
2018-09-21 12:49:10 +0000

    build and run docker image

--- invoicemanager/settings.py
+++ invoicemanager/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.33.10']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
