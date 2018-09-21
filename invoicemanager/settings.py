3a3af3eeef7010b08c2b28d0613fadf4ef732736
Williano <paawilly17@gmai.com>
2018-09-21 19:32:29 +0000

    [UPDATE]:Change debug back to True
    Changed it to False to test if the page not found or server error page
    work working

--- invoicemanager/settings.py
+++ invoicemanager/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%d#l!8i_*!qb!bd9i)^ie86a5x^4z6e!mu-03l%ujt54g72p&m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*', ]
+ALLOWED_HOSTS = []
 
 
 # Application definition
