b564c75c80a1ccc50efbceb2279701b16bccd6d9
Anthony Abeo <anthonyabeo@gmail.com>
2018-09-26 15:20:15 +0000

    manually running networked containers

--- users/urls.py
+++ users/urls.py
@@ -4,6 +4,6 @@ from users.views import registration, sign_in, sign_out
 
 urlpatterns = [
     url(r'^registration/$', registration, name="registration"),
-    url(r'^login/$', sign_in, name="login"),
+    url(r'^$', sign_in, name="login"),
     url(r'^logout/$', sign_out, name="logout"),
 ]
