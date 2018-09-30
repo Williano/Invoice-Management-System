8428868973a8573cb542be1dd002f1be6951af78
Anthony Abeo <anthonyabeo@gmail.com>
2018-09-30 19:10:57 +0000

    fix tests import error

--- invoicemanager/settings.py
+++ invoicemanager/settings.py
@@ -88,18 +88,18 @@ WSGI_APPLICATION = 'invoicemanager.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ['NAME'],
-        'USER': os.environ['USER'],
-        'PASSWORD': os.environ['PASSWORD'],
-        'HOST': os.environ['HOST'],
-        'PORT': os.environ['PORT'],
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': os.environ['NAME'],
+    #     'USER': os.environ['USER'],
+    #     'PASSWORD': os.environ['PASSWORD'],
+    #     'HOST': os.environ['HOST'],
+    #     'PORT': os.environ['PORT'],
+    # }
 }
 
 
