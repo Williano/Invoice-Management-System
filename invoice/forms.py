dfcd0d84c013e563d7ab15c91b0442264fe66c31
nafiu <nshaibu>
2018-10-01 15:16:59 +0000

    Added the functionality of autofield update and validation of form fields before posting the data

--- invoice/forms.py
+++ invoice/forms.py
@@ -8,6 +8,6 @@ class ItemForms(forms.Form):
     item = forms.CharField(min_length=2, max_length=100, widget=forms.TextInput(attrs={'class':'form-control'}))
     description = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
     cost = forms.DecimalField(widget=forms.TextInput(attrs={'placeholder':'0.00', 'class': 'form-control' }))
-    qty = forms.IntegerField(min_value=1, initial=1)
+    qty = forms.IntegerField(min_value=1)
 
 ItemFormset = formset_factory(ItemForms, extra=1)
