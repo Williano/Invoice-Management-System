e58f7d8c12ba5b201dc8c5dcf912a15841938d40
Williano <paawilly17@gmail.com>
2018-10-04 13:50:02 +0000

    [REFACTOR] :hammer: Refactor cutomer functionality
    The customer models fields were throwing errors.
    The customer fields were throwing errors because the database had
    was not set up to accept null data.
    
    This commit fix this error by setting nulled equals true for
    the cutomer fields.
    This commit also change the customer models fields and also refactored
    the customer app to use django forms. This helps to check for
    invalid fields.
    
    This commit also change the updates the customer fields in the
    templates.

--- invoice/forms.py
+++ invoice/forms.py
@@ -2,12 +2,33 @@ from __future__ import unicode_literals
 
 from django import forms
 from django.forms import formset_factory
+from .models.customer import Customer
+
+
+class CustomerForm(forms.ModelForm):
+    """
+        Forms for creating a new customer.
+    """
+    class Meta:
+        model = Customer
+        fields = ['name', 'address', 'city', 'region', 'country', 'email', ]
+        widgets = {'name': forms.TextInput(attrs={"class": "form-control"}),
+                   'address': forms.TextInput(attrs={"class": "form-control"}),
+                   'city': forms.TextInput(attrs={"class": "form-control"}),
+                   'region': forms.TextInput(attrs={"class": "form-control"}),
+                   'country': forms.TextInput(attrs={"class": "form-control"}),
+                   'email': forms.TextInput(attrs={"class": "form-control"}),
+                   }
 
 
 class ItemForms(forms.Form):
-    item = forms.CharField(min_length=2, max_length=100, widget=forms.TextInput(attrs={'class':'form-control'}))
-    description = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
-    cost = forms.DecimalField(widget=forms.TextInput(attrs={'placeholder':'0.00', 'class': 'form-control' }))
+    item = forms.CharField(min_length=2, max_length=100, widget=forms.TextInput(
+        attrs={'class': 'form-control'}))
+    description = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control'}))
+    cost = forms.DecimalField(
+        widget=forms.TextInput(
+            attrs={'placeholder': '0.00', 'class': 'form-control' }))
     qty = forms.IntegerField(min_value=1)
 
 
--- invoice/views/customers.py
+++ invoice/views/customers.py
@@ -1,15 +1,16 @@
-import datetime
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
-from django.urls import reverse
-from django.contrib import messages
+# Third Party imports
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render, redirect
+from django.urls import reverse
 
-# from invoice.models import Customer, Invoice, InvoiceItem
+# Local Applications imports
 from invoice.models.customer import Customer
 from invoice.models.inv import Invoice
-from invoice.models.invoice_item import InvoiceItem
+from invoice.forms import CustomerForm
+
 
 @login_required(login_url='users:login')
 def customer_list(request):
@@ -30,8 +31,8 @@ def customer_list(request):
     except EmptyPage:
         customers = paginator.page(paginator.num_pages)
     context = {
-        'title' : 'Customer List',
-        'customers' : customers,
+        'title': 'Customer List',
+        'customers': customers,
     }
     return render(request, 'invoice/customers.html', context)
 
@@ -44,58 +45,51 @@ def customer(request, customer_id):
 
     """
     customer = get_object_or_404(Customer, pk=customer_id)
-    invoices = Invoice.objects.filter(customer = customer).order_by('-date_created')
+    invoices = Invoice.objects.filter(customer=customer).order_by('-date_created')
     context = {
-        'title' : "Customer info - %s" % customer.name,
-        'customer' : customer,
-        'invoices' : invoices,
+        'title': "Customer info - %s" % customer.name,
+        'customer': customer,
+        'invoices': invoices,
     }
     return render(request, 'invoice/customer.html', context)
 
 
-# Add new customer
+# View for creating a new customer.
 @login_required(login_url='users:login')
 def new_customer(request):
     if request.method == 'POST':
-        # Stuff from form
-        c = Customer(name=request.POST['name'],
-                     address1=request.POST['address1'],
-                     address2=request.POST['address2'],
-                     city=request.POST['city'], state=request.POST['state'],
-                     zip=request.POST['zip'], email=request.POST['email'])
-        c.save()
-
-        if 'savecreate' in request.POST:
-            i = Invoice(customer=c, expiration_date=datetime.date.today(),
-                        status='Unpaid')
-            i.save()
-            messages.success(request, 'New Customer successfully created! ')
-            return HttpResponseRedirect(
-                reverse('invoice:invoice', args=(i.id,)))
-        else:
-            return HttpResponseRedirect(reverse('invoice:customer_list'))
+        form = CustomerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'New Customer created successfully')
+            return redirect('invoice:customer_list')
     else:
-        return render(request, 'invoice/new_customer.html')
+        form = CustomerForm()
+
+    context = {'form': form}
+    template_name = 'invoice/new_customer.html'
+
+    return render(request, template_name, context)
 
 
 # Update customer
 @login_required(login_url='users:login')
 def update_customer(request, customer_id):
-    # Stuff from form
-    c = get_object_or_404(Customer, pk=customer_id)
-
-    c.name = request.POST['name']
-    c.address1 = request.POST['address1']
-    c.address2 = request.POST['address2']
-    c.city = request.POST['city']
-    c.state = request.POST['state']
-    c.zip = request.POST['zip']
-    c.email = request.POST['email']
-
-    c.save()
-    messages.success(request, 'Customer Details Updated Successfully')
-
-    return HttpResponseRedirect(reverse('invoice:customer', args=(c.id,)))
+    customer = get_object_or_404(Customer, pk=customer_id)
+    if request.method == 'POST':
+        form = CustomerForm(request.POST, instance=customer)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Customer details updated successfully')
+            return HttpResponseRedirect(reverse('invoice:customer',
+                                                args=(customer.id,)))
+    else:
+        form = CustomerForm(instance=customer)
+    context = {
+        'form': form,
+    }
+    template_name = 'invoice/customer.html'
+    return render(request, template_name, context)
 
 
 # Delete customer
@@ -103,4 +97,5 @@ def update_customer(request, customer_id):
 def delete_customer(request, customer_id):
     customer = get_object_or_404(Customer, pk=customer_id)
     customer.delete()
+    messages.success(request, "Customer successfully deleted.")
     return HttpResponseRedirect(reverse('invoice:customer_list'))
