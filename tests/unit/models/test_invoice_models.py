17addd513516bd6a5d5f9e4e0d6d26c299a3e0d6
Williano <paawilly17@gmail.com>
2018-10-05 10:32:47 +0000

    [TEST] :white_check_mark: Add test for invoice model methods

--- tests/unit/models/test_invoice_models.py
+++ tests/unit/models/test_invoice_models.py
@@ -10,7 +10,7 @@ from invoice.models.invoice_item import InvoiceItem
 # models test
 class InvoiceModelTest(TestCase):
     def setUp(self):
-        c1 = Customer.objects.create(
+        customer1 = Customer.objects.create(
             name="Mpedigree",
             address="P.O.Box KS 10731",
             city="Kumasi",
@@ -19,7 +19,7 @@ class InvoiceModelTest(TestCase):
             email="paawilly17@gmail.com",
         )
 
-        c2 = Customer.objects.create(
+        customer2 = Customer.objects.create(
             name="JimahTech",
             address="P.O.Box KS 10731",
             city="Kumasi",
@@ -28,30 +28,30 @@ class InvoiceModelTest(TestCase):
             email="paawilly18@gmail.com",
         )
 
-        inv1 = Invoice.objects.create(
-            customer=c1,
+        invoice1 = Invoice.objects.create(
+            customer=customer1,
             invoice_code="b0aa7e49-8565-42c6-856b-4f1b03205d77",
             valid=True,
             date_created="09-28-2018",
             expiration_date="2018-09-29",
-            status="unpaid",
+            status="Unpaid",
 
 
         )
 
-        inv2 = Invoice.objects.create(
-            customer=c2,
+        invoice2 = Invoice.objects.create(
+            customer=customer2,
             invoice_code="b0aa7e49-8565-42c6-856b-4f1b03205d88",
             valid=True,
             date_created="2018-09-29",
             expiration_date="2018-10-29",
-            status="paid",
+            status="Paid",
 
 
         )
 
         InvoiceItem.objects.create(
-            invoice=inv1,
+            invoice=invoice1,
             name="Software Books",
             description="new item for business2",
             cost=50,
@@ -59,7 +59,7 @@ class InvoiceModelTest(TestCase):
         )
 
         InvoiceItem.objects.create(
-            invoice=inv1,
+            invoice=invoice1,
             name="Software",
             description="new item for business",
             cost=50,
@@ -67,44 +67,36 @@ class InvoiceModelTest(TestCase):
               )
 
         InvoiceItem.objects.create(
-            invoice=inv2,
+            invoice=invoice2,
             name="Software come",
             description="new item for business3",
             cost=20,
             qty=20
               )
 
-    def test_customer_content(self):
+    def test_customer_name(self):
         customer = Customer.objects.get(id=1)
         expected_object_name = "%s" % customer.name
         self.assertEquals(expected_object_name, customer.__str__())
 
-    def test_customer_content_with_invalid_name(self):
-        customer = Customer.objects.get(id=2)
-        expected_object_name = "%s" % customer.name
-        self.assertEquals(expected_object_name, customer.__str__())
-
-    def test_invoice_content(self):
+    def test_invoice_id(self):
         invoice = Invoice.objects.get(id=1)
         expected_object_name = "%s" % invoice.id
         self.assertEquals(expected_object_name, invoice.__str__())
 
-    def test_invoice_content_with_invalid_name(self):
-        invoice = Invoice.objects.get(id=2)
-        expected_object_name = "%s" % invoice.id
-        self.assertEquals(expected_object_name, invoice.__str__())
-
-    def test_invoice_item_one_total_price(self):
-
+    def test_total_price_for_invoice_item_one(self):
         invoice_item = InvoiceItem.objects.get(id=1)
-        total_price = invoice_item.cost * invoice_item.qty
-        self.assertEqual(total_price, 1000)
+        self.assertEqual(invoice_item.total(), 1000)
 
-    def test_invoice_item_two_total_price(self):
+    def test_total_price_for_invoice_item_two(self):
         invoice_item = InvoiceItem.objects.get(id=3)
-        total_price = invoice_item.cost * invoice_item.qty
-        self.assertEqual(total_price, 400)
+        self.assertEqual(invoice_item.total(), 400)
 
-    def test_unpaid_invoice(self):
-        invoice_unpaid = Invoice.objects.filter(status="unpaid").count()
-        self.assertEqual(invoice_unpaid, 1)
+    def test_total_items_in_an_invoice(self):
+        invoice = Invoice.objects.get(id=1)
+        invoice_item = invoice.invoiceitem_set.all().count()
+        self.assertEqual(invoice_item, 2)
+
+    def test_total_cost_of_an_invoice(self):
+        invoice = Invoice.objects.get(id=1)
+        self.assertEqual(invoice.total(), 2000)
